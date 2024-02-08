79cd05455f1514650648765222873db95d491270
yakumo22-r <2358044108@qq.com>
2024-02-09 03:35:12 +0800

    move user.lua config

--- user.lua
+++ user.lua
@@ -1,25 +1,47 @@
-local user = {}
+local wezterm = require 'wezterm'
+local filepath = wezterm.home_dir.."/.config/wezterm-user.lua"
 
--- add ssh connect here
-user.ssh = 
-{
-	{label = "example-ssh", args = { 'ssh','example-server'}},
-}
+local file = io.open(filepath, "r")
+if file then
+	file:close()
+else
+	file = io.open(filepath, "w")
+	local default_content = [[
+	-- add ssh connect here
+	local user = {}
+	user.ssh =
+	{
+		{label = "sample-ssh", args = { 'ssh','sample-ssh'}},
+	}
 
-user.window = 
-{
-	-- max chars horizontal
-	width = 120,
-	-- max chars verticle
-	height = 60,
+	user.window =
+	{
+		-- max chars horizontal
+		width = 140,
+		-- max chars verticle
+		height = 40,
 
-	-- https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-	font = "JetBrainsMono Nerd Font Propo",
-	fontsize = 18,
+		-- https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+		font = "JetBrainsMono Nerd Font Propo",
+		fontsize = 18,
 
-	-- image file under backdrops/
-	background = 'shermie.png',
-}
+		-- image file under backdrops/
+		background = 'shermie.png',
+	}
+	return user
+	]]
+	if(file)then
+		file:write(default_content)
+		file:close()
+	else
+		print('cannot open file for writting: '..filepath)
+	end
+end
+
+local originalPath = package.path
+package.path = filepath..";" .. package.path
+local user = require("wezterm-user")
+package.path = originalPath
 
 
 return user;
